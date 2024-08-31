c592a51ec9afde28f7d0f9b5da8d5c614694dbc8
hinzwifi <markjoshuahaynes@protonmail.com>
2024-08-31 12:44:03 +0800

    Idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navigation />
           {children}
         </ThemeProvider>
         <Toaster />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import DarkPage from "@/components/DarkPage";
+import Navigation from "@/components/NavBar";
 
 export default function Home() {
-  return <DarkPage />;
+  return (
+    <>
+      <Navigation />
+      <DarkPage />
+    </>
+  );
 }
